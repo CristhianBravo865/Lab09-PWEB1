e493b85d935e19ec096e012bdd1ca5414a5b3cd3
Cristhian <cristhian.bravo.8657@gmail.com>
2024-11-09 19:15:19 -0500

    Filtros de busqueda perl

--- cgi-bin/consultauniversidades.pl
+++ cgi-bin/consultauniversidades.pl
@@ -5,8 +5,12 @@ use CGI qw(:standard);
 use CGI::Carp 'fatalsToBrowser';
 
 my $file_path = './data/Programas_de_Universidades_8.csv';
-
 my $cgi = CGI->new;
+
+my $nombre = $cgi->param('nombre') || '';
+my $periodo = $cgi->param('periodo') || '';
+my $departamento = $cgi->param('departamento') || '';
+my $programa = $cgi->param('programa') || '';
 
 print $cgi->header(-type => 'text/plain', -charset => 'UTF-8');
 
@@ -16,5 +20,24 @@ if (! -e $file_path) {
 }
 
 open(my $fh, '<:encoding(ISO-8859-1)', $file_path) or die "No se puede abrir el archivo: $!";
-print "Archivo abierto correctamente.";
+
+my @resultados;
+while (my $line = <$fh>) {
+    chomp $line;
+    my @campos = split(/\|/, $line);
+
+    if (($nombre eq '' || $campos[1] =~ /\Q$nombre\E/i) &&
+        ($periodo eq '' || $campos[6] =~ /\Q$periodo\E/i) &&
+        ($departamento eq '' || $campos[12] =~ /\Q$departamento\E/i) &&
+        ($programa eq '' || $campos[18] =~ /\Q$programa\E/i)) {
+
+        push @resultados, join(", ", @campos[1, 6, 12, 18]);
+    }
+}
 close $fh;
+
+if (@resultados) {
+    print join("\n", @resultados);
+} else {
+    print "No se encontraron resultados para los criterios especificados.";
+}
